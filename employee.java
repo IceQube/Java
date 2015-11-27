6114ab57f54a91b3695c6a52664ebedb81b40f7e
IceQube <onons2018@student.vis.ac.at>
2015-11-27 11:23:34 +0100

    Update employee.java

--- employee.java
+++ employee.java
@@ -3,6 +3,21 @@ class employee{
     int age;
     double Salary;
     
-    public static void setName(int name){
-      
+    public static void setName(String name){
+      name = name;
     }
+    public static void setDesignation(String designation){
+      designation = designation;
+    }
+        public static void setAge(int Age){
+      age = age;
+    }
+    
+        public static void setSalary(double salary){
+      salary = salary;
+      
+      public static String getName(){
+          return name;
+      }
+    }
+}
